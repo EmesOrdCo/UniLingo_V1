a43f5ccb741495c3baaad47fc925e1820edcaaa7
Haza1410 <harry.emes@hotmail.com>
2025-10-16 13:46:33 +0100

    Fix VoiceService AWSPollyService import
    - Changed from default import to named import
    - Fixes 'Cannot read property playSpeech of undefined' error
    - VoiceService now properly calls AWSPollyService.playSpeech()

--- src/lib/voiceService.ts
+++ src/lib/voiceService.ts
@@ -1,6 +1,6 @@
 import * as Speech from 'expo-speech';
 import { ENV } from './envConfig';
-import AWSPollyService from './awsPollyService';
+import { AWSPollyService } from './awsPollyService';
 
 export interface VoiceServiceConfig {
   language?: string;
